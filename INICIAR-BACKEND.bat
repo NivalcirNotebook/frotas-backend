4d2bffa342f4dbf0ae89b832066d5b0ec3fbfa3f
Nivalcir <nivalcir.souza@hotmail.com>
2026-03-21 22:28:59 -0300

    Correções

---
+++ INICIAR-BACKEND.bat
@@ -0,0 +1,7 @@
+@echo off
+title Backend - Porta 3002
+echo ========================================
+echo INICIANDO BACKEND (Porta 3002)
+echo ========================================
+echo.
+npm start
